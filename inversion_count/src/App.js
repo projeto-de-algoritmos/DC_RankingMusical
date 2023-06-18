1cc6306a334fc14cb9e7c5305a72b2ed25926d3f
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2023-06-18 20:31:05 -0300

    feat: implementa cálculo das inversões

--- inversion_count/src/App.js
+++ inversion_count/src/App.js
@@ -2,45 +2,65 @@ import React from 'react';
 
 import './styles/App.css'
 import Board from './Board'
+import incredible0 from './mrIncredible/0.png'
+import incredible1 from './mrIncredible/1.png'
+import incredible2 from './mrIncredible/2.png'
+import incredible3 from './mrIncredible/3.png'
+import incredible4 from './mrIncredible/4.png'
 
-function countInversions(arr) {
-  let count = 0;
-
-  function mergeSort(arr) {
-    if (arr.length <= 1) {
-      return arr;
-    }
-
-    const mid = Math.floor(arr.length / 2);
-    const left = mergeSort(arr.slice(0, mid));
-    const right = mergeSort(arr.slice(mid));
-
-    return merge(left, right);
+function sortAndCount(L) {
+  if (L.length <= 1) {
+    return 0;
   }
 
-  function merge(left, right) {
-    let merged = [];
-    let i = 0;
-    let j = 0;
+  const mid = Math.floor(L.length / 2);
+  const A = L.slice(0, mid);
+  const B = L.slice(mid);
 
-    while (i < left.length && j < right.length) {
-      if (left[i] <= right[j]) {
-        merged.push(left[i]);
-        i++;
-      } else {
-        merged.push(right[j]);
-        count += left.length - i; // Increase count by the number of remaining elements in the left subarray
-        j++;
-      }
-    }
+  const rA = sortAndCount(A);
+  const rB = sortAndCount(B);
 
-    return merged.concat(left.slice(i)).concat(right.slice(j));
-  }
+  const r = mergeAndCount(A, B);
 
-  mergeSort(arr);
-
-  return count;
+  return rA + rB + r;
 }
+
+function mergeAndCount(A, B) {
+  let i = 0;
+  let j = 0;
+  let inversions = 0;
+
+  while (i < A.length && j < B.length) {
+    if (A[i] <= B[j]) {
+      i++;
+    } else {
+      inversions += A.length - i;
+      j++;
+    }
+  }
+
+  return inversions;
+}
+
+function getMusicRank(myMusicRank, yourMusicRank) {
+  return Object.entries(yourMusicRank).map(([key, value]) => myMusicRank[key].pos);
+}
+
+function getCompatibilityScale(inversionCount) {
+  if (inversionCount >= 0 && inversionCount <= 10) {
+    return 0; // Low Compatibility
+  } else if (inversionCount >= 11 && inversionCount <= 20) {
+    return 1; // Moderate Compatibility
+  } else if (inversionCount >= 21 && inversionCount <= 30) {
+    return 2; // Average Compatibility
+  } else if (inversionCount >= 31 && inversionCount <= 40) {
+    return 3; // High Compatibility
+  } else {
+    return 4; // Very High Compatibility
+  }
+}
+
+const mrIncredible = [incredible0, incredible1, incredible2, incredible3, incredible4]
 
 class App extends React.Component {
   constructor(props){
@@ -75,29 +95,36 @@ class App extends React.Component {
   }
 
   handleMyStateChange = (newState) => {
+    let count  = sortAndCount(getMusicRank(newState, this.state.yourMusicRank))
     this.setState({
       myMusicRank: newState
     })
     this.setState({
-      // inversions: countInversions
+      inversions: count
     })
   };
   handleYoursStateChange = (newState) => {
+    let count  = sortAndCount(getMusicRank(this.state.yourMusicRank, newState))
     this.setState({
       yourMusicRank: newState
     })
     this.setState({
-      // inversions: countInversions
+      inversions: count
     })
+
   };
 
   render(){
     return (
       <div className="app">
+        <button onClick={()=> console.log(this.state)}>Teste</button>
         <h1 className='large rise'>Ranking de gêneros musicais!</h1>
-        <button onClick={()=> {console.log(this.state)}}>Teste</button>
         <Board onStateChange={this.handleMyStateChange}/>
         <Board onStateChange={this.handleYoursStateChange}/>
+        <div className="result">
+          <div className="mrIncredible" style={{ backgroundImage: `url(${mrIncredible[getCompatibilityScale(this.state.inversions)]})`}}></div>
+          <div className="inversionsNumber">{this.state.inversions} Inversões</div>
+        </div>
       </div>
     );
   }
